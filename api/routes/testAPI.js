7c9875d66f2f4862230b031328385e6cf4110a94
dolkaiser <43217783+dolkaiser@users.noreply.github.com>
2019-11-07 02:31:54 +0900

    DB connection test

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,31 @@
 var express = require("express");
+var mysql = require('mysql');
 var router = express.Router();
 
+var pool = mysql.createPool({
+	host : 'localhost',
+	port : 3306,
+	user : 'root',
+	password : '!sodwkdrh1234',
+	database : 'bueno'
+  })
+
 router.get("/", function(req, res, next){
-	res.send("API is working properly");
+	//res.send("API is working properly");
+	pool.getConnection((err, connection)=>{
+		if(err) throw err;
+		else{
+		  connection.query("select * from user", (err, rows) => {
+			if(err) throw err;
+			else
+			{
+			  console.log(rows);
+			  res.send(rows);
+			}
+		  })
+		  connection.release();
+		}
+	  })
 });
 
 module.exports = router;
