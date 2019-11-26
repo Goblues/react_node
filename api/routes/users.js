c3c849788316a5c071cc6f693c5f382cb7fb73c6
Goblues <jju2517@gmail.com>
2019-11-26 18:16:09 +0900

    good2

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get("/", function(req, res) {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "Jung JIn Woo",
+      text: "yoga"
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "Lee Young Hyun",
+      text: "Pilates"
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "Seo Jong Min",
+      text: "G.X"
+    }
+  ]);
 });
 
 module.exports = router;
